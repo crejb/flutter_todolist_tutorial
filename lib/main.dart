8895e69475bdfe693f031d720b87fd20c823573d
Christopher Bowles <Christopher.Bowles@globalx.com.au>
2021-07-28 14:48:14 +1000

    refactor to use providers for state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,22 +11,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: ToDoList(title: 'My To Do List'),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => ToDoListModel(),
+        child: MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: ToDoList(title: 'My To Do List')));
   }
 }
 
@@ -46,61 +41,67 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
-  List<ToDoItem> _toDoItems = [];
   final ScrollController _scrollController = ScrollController();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(widget.title)),
-        body: getBody(),
-        floatingActionButton: FloatingActionButton(
-            onPressed: openAddItemDialog, child: Icon(Icons.add)));
+  void didChangeDependencies() {
+    super.didChangeDependencies();
   }
 
-  Widget getBody() {
-    if (_toDoItems.length > 0) {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text(widget.title),
+                bottom: const TabBar(
+                    tabs: [Tab(text: 'All Items'), Tab(text: 'Priority')])),
+            body:
+                TabBarView(children: [getItemsBody(), getPriorityItemsBody()]),
+            floatingActionButton: FloatingActionButton(
+                onPressed: openAddItemDialog, child: Icon(Icons.add))));
+  }
+
+  Widget getItemsBody() {
+    return Consumer<ToDoListModel>(builder: (context, model, child) {
+      if (model.itemCount > 0) {
+        return Scrollbar(
+            isAlwaysShown: false,
+            controller: _scrollController,
+            child: ListView(
+                children: ListTile.divideTiles(
+                    context: context,
+                    tiles: model.toDoItems
+                        .map((item) => ToDoListItem(item: item))).toList()));
+      }
+      return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text('You haven\'t added any items yet!'));
+    });
+  }
+
+  Widget getPriorityItemsBody() {
+    return Consumer<ToDoListModel>(builder: (context, model, child) {
       return Scrollbar(
           isAlwaysShown: false,
           controller: _scrollController,
           child: ListView(
               children: ListTile.divideTiles(
                   context: context,
-                  tiles: _toDoItems.map((item) => ToDoListItem(
-                      item: item, onItemChanged: onItemChanged))).toList()));
-    }
-    return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text('You haven\'t added any items yet!'));
+                  tiles: model.toDoItems
+                      .where((item) => model.isFavourite(item))
+                      .map((item) => ToDoListItem(item: item))).toList()));
+    });
   }
 
   void openAddItemDialog() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      return AddToDoItemPage(onAddItemCompleted: addItem);
-    }));
-  }
-
-  void addItem(String title) {
-    setState(() {
-      final item = ToDoItem(title: title, isDone: false);
-      _toDoItems.add(item);
-    });
-  }
-
-  void onItemChanged(ToDoItem item) {
-    setState(() {
-      _toDoItems[_toDoItems.indexOf(item)] = item.toggleDone();
-    });
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) => AddToDoItemPage()));
   }
 }
 
-typedef void ItemAddedCallback(String title);
-
 class AddToDoItemPage extends StatefulWidget {
-  final ItemAddedCallback onAddItemCompleted;
-  AddToDoItemPage({required this.onAddItemCompleted});
-
   @override
   _AddToDoItemPageState createState() => _AddToDoItemPageState();
 }
@@ -110,6 +111,7 @@ class _AddToDoItemPageState extends State<AddToDoItemPage> {
 
   @override
   Widget build(BuildContext context) {
+    final model = Provider.of<ToDoListModel>(context, listen: false);
     return Scaffold(
         appBar: AppBar(title: const Text('Add New Task')),
         body: Padding(
@@ -122,7 +124,7 @@ class _AddToDoItemPageState extends State<AddToDoItemPage> {
                   )),
               ElevatedButton(
                   onPressed: () {
-                    this.widget.onAddItemCompleted(myController.text);
+                    model.add(ToDoItem(title: myController.text));
                     Navigator.of(context).pop();
                   },
                   child: Text('Add'))
@@ -132,32 +134,137 @@ class _AddToDoItemPageState extends State<AddToDoItemPage> {
 
 class ToDoItem {
   final String title;
-  final bool isDone;
-  ToDoItem({required this.title, required this.isDone});
-
-  ToDoItem toggleDone() {
-    return ToDoItem(title: this.title, isDone: !this.isDone);
-  }
+  ToDoItem({required this.title});
 }
-
-typedef void ItemDoneChangedCallback(ToDoItem item);
 
 class ToDoListItem extends StatelessWidget {
   final ToDoItem item;
-  final ItemDoneChangedCallback onItemChanged;
 
-  ToDoListItem({required this.item, required this.onItemChanged});
+  ToDoListItem({required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        onTap: () {
-          onItemChanged(item);
-        },
-        leading: item.isDone
-            ? Icon(Icons.check_box, color: Colors.green)
-            : Icon(Icons.check_box_outline_blank),
-        title: Text(item.title,
-            style: item.isDone ? TextStyle(color: Colors.black26) : null));
+    return Consumer<ToDoListModel>(builder: (context, model, child) {
+      final isDone = model.isDone(item);
+      final isFavourite = model.isFavourite(item);
+
+      var toggleDone = () {
+        if (isDone) {
+          model.unmarkDone(item);
+        } else {
+          model.markDone(item);
+        }
+      };
+
+      var toggleFavourite = () {
+        if (isFavourite) {
+          model.unmarkFavourite(item);
+        } else {
+          model.markFavourite(item);
+        }
+      };
+
+      var removeItem = () {
+        model.remove(item);
+      };
+
+      return ListTile(
+          onTap: () {},
+          leading: isDone
+              ? IconButton(
+                  icon: Icon(Icons.check_box, color: Colors.green),
+                  onPressed: toggleDone)
+              : IconButton(
+                  icon: Icon(Icons.check_box_outline_blank),
+                  onPressed: toggleDone),
+          title: Text(item.title,
+              style: isDone ? TextStyle(color: Colors.black26) : null),
+          //trailing: buildFavouriteButton(model, item));
+          trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+            buildFavouriteButton(model, item),
+            buildRemoveButton(model, item)
+          ]));
+      // trailing: Row(children: [
+      //   isFavourite
+      //       ? IconButton(
+      //           icon: Icon(Icons.favorite, color: Colors.red),
+      //           onPressed: toggleFavourite)
+      //       : IconButton(
+      //           icon: Icon(Icons.favorite_outline),
+      //           onPressed: toggleFavourite),
+      //   IconButton(icon: Icon(Icons.remove_circle), onPressed: removeItem)
+      // ]));
+    });
+  }
+
+  Widget buildFavouriteButton(ToDoListModel model, ToDoItem item) {
+    return model.isFavourite(item)
+        ? IconButton(
+            icon: Icon(Icons.favorite, color: Colors.red),
+            onPressed: () => model.unmarkFavourite(item))
+        : IconButton(
+            icon: Icon(Icons.favorite_outline),
+            onPressed: () => model.markFavourite(item));
+  }
+
+  Widget buildRemoveButton(ToDoListModel model, ToDoItem item) {
+    return IconButton(
+        icon: Icon(Icons.remove_circle), onPressed: () => model.remove(item));
+  }
+}
+
+class ToDoListModel extends ChangeNotifier {
+  final List<ToDoItem> _toDoItems = [];
+  final Set<ToDoItem> _completedItems = {};
+  final Set<ToDoItem> _favouriteItems = {};
+
+  UnmodifiableListView<ToDoItem> get toDoItems =>
+      UnmodifiableListView(_toDoItems);
+
+  int get itemCount => _toDoItems.length;
+
+  void add(ToDoItem item) {
+    _toDoItems.add(item);
+    notifyListeners();
+  }
+
+  void remove(ToDoItem item) {
+    if (_toDoItems.remove(item)) {
+      _completedItems.remove(item);
+      _favouriteItems.remove(item);
+      notifyListeners();
+    }
+  }
+
+  void markDone(ToDoItem item) {
+    if (_completedItems.add(item)) {
+      notifyListeners();
+    }
+  }
+
+  void unmarkDone(ToDoItem item) {
+    if (_completedItems.remove(item)) {
+      notifyListeners();
+    }
+  }
+
+  bool isDone(ToDoItem item) {
+    return _completedItems.contains(item);
+  }
+
+  bool isFavourite(ToDoItem item) {
+    return _favouriteItems.contains(item);
+  }
+
+  void markFavourite(ToDoItem item) {
+    if (_favouriteItems.add(item)) {
+      notifyListeners();
+    }
+  }
+
+  void unmarkFavourite(ToDoItem item) {
+    if (_favouriteItems.remove(item)) {
+      notifyListeners();
+    }
   }
 }
